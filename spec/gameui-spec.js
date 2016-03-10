e7ebaa94184084f556d5e265137e3a783c408355
vosbek <vcgoat@gmail.com>
2016-03-10 13:12:05 -0500

    listeners, and defenders and attackers and stuff

--- spec/gameui-spec.js
+++ spec/gameui-spec.js
@@ -1,25 +1,32 @@
 describe("Game UI ", function(){
-	var missionString = "<mission></mission>";
+	var missionString = '<mission></mission>';
 	var $compile;
 	var $rootScope;
 	var gameInstance;
+	var listeners;
 
 	beforeEach(function(){
 		module('app');
 	});
 
 	 
-	beforeEach(inject(function(_$compile_, _$rootScope_, _game_){
+	beforeEach(inject(function(_$compile_, _$rootScope_, _game_, _listeners_){
     $compile = _$compile_;
     $rootScope = _$rootScope_;
     gameInstance = _game_;
+    listeners = _listeners_;
   })); 
 
 	it('starts mission', function(){
 		var element = $compile(missionString)($rootScope);
+		$rootScope.missionLog = [];
+		listeners.attack.scope = $rootScope;
+		listeners.defend.scope = $rootScope;
 		$rootScope.$digest();
-		element.triggerHandler("click");
+		//element.triggerHandler("click");
+		//gameInstance.startMission();
+		//toDo figure out how t click a button 
 		gameInstance.increment();
-		expect(element.html()).toContain("attack");
+		expect($rootScope.missionLog[0]).toEqual("attack");
 	});
 });
