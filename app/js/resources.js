cfcc37109e34bb81dd36b1daf76c3ca9abd508a3
Utter <utterc2@nationwide.com>
2016-06-01 12:11:14 -0400

    worked on copper resource test

--- app/js/resources.js
+++ app/js/resources.js
@@ -1,7 +1,12 @@
 var Resources = function(){
-this.science = 0;
+this.science = 0
+this.copper = 0
 
   this.addScience = function(incomingScience){
       this.science += incomingScience;
   }
+
+    this.addCopper = function(copperAmount){
+        this.copper += copperAmount;
+    }
 }
--- spec/resources-spec.js
+++ spec/resources-spec.js
@@ -1,13 +1,23 @@
 describe("resources", function(){
+var randomizer = new Randomizer()
 var resources;
 var newScience = 1
+var copperAmount = randomizer.randomize(10)
+
   it("adds science to total science", function(){
     resources = new Resources();
 
     resources.addScience(newScience);
 
     expect(resources.science).toEqual(1);
-  })
+  });
+  
+  it("copper resource gets added to copper total", function(){
+    resources = new Resources();
+    resources.copper = 10;
 
+    resources.addCopper(copperAmount);
 
+    expect(resources.copper).toEqual(10 + copperAmount);
+  });
 })
