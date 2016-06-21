076da6a88c4dbf363fe17930fae4ef3e8453fa4c
Utter <utterc2@nationwide.com>
2016-06-21 14:25:33 -0400

    added features to display Heroes Health with progress bar

--- app/js/cloningFacility.js
+++ app/js/cloningFacility.js
@@ -1,7 +1,12 @@
-var CloningFacility = function(battle) {
-  var baseHealth = 10;
-  var baseAttack = 10;
-  this.receiveEvent = function(message) {
-    return battle.loadNewAttacker(new CombatParty(baseHealth, baseAttack, Mediator.getInstance(), "Redshirt"+Math.floor(Math.random()*100)+1, "hero"));
-  }
+var CloningFacility = function (battle) {
+   var baseHealth = 10;
+   var baseAttack = 10;
+   this.randomizer = new Randomizer();
+   this.receiveEvent = function (message) {
+      var heroName = "Redshirt" + this.randomizer.randomize(100);
+      this.scope.heroName = heroName;
+      this.scope.heroHealth = baseHealth;
+      this.scope.heroMaxHealth = baseHealth;
+      return battle.loadNewAttacker(new CombatParty(baseHealth, baseAttack, Mediator.getInstance(), heroName, "hero"));
+   }
 }
--- spec/cloningFacility-spec.js
+++ spec/cloningFacility-spec.js
@@ -1,6 +1,29 @@
-describe("cloningFacility", function(){
-   
-   it("adds hero name to UI", function(){
-         fail;
+describe("cloningFacility", function () {
+
+   var message = td.object("AttackMessage");
+   var battle = td.object("Battle");
+   var randomizer = td.object("Randomizer");
+   var cloningFacility = new CloningFacility(battle);
+
+   beforeEach(inject(function ($rootScope) {
+      cloningFacility.scope = $rootScope.$new();
+      cloningFacility.scope.heroName = "";
+      cloningFacility.scope.heroHealth = "";
+   }));
+
+   it("adds hero name to UI", function () {
+      td.when(randomizer.randomize(td.matchers.isA(Number))).thenReturn(65);
+      cloningFacility.randomizer = randomizer;
+      cloningFacility.receiveEvent(message);
+
+      expect(cloningFacility.scope.heroName).toEqual("Redshirt65");
    });
+
+   it("adds hero health to UI", function(){
+      cloningFacility.receiveEvent(message);
+
+      expect(cloningFacility.scope.heroHealth).toEqual(10);
+   });
+
 });
+
